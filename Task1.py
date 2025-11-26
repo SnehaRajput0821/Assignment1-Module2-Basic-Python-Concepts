846c2b35375fdb5079f20a4895514578934c30e4
SnehaRajput0821 <sneharajput.0821@gmail.com>
2025-11-26 10:39:01 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,11 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+sum=num1+num2
+multi=num1*num2
+div=num2/num1
+sub=num2-num1
+print("Calculation is as follows")
+print("The sum of two numbers are: ", sum)
+print("The multi of two numbers are: ", multi)
+print("The div of two numbers are: ", div)
+print("The sub of two numbers are: ", sub)
---
+++ Task2.py
@@ -0,0 +1,3 @@
+first_name=str(input("Enter your first name: "))
+last_name=str(input("Enter your last name name: "))
+print("Hello",first_name + " "+last_name, "Welcome to the first python program")
